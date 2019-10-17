28643e5c1b54cd7db3fc59586c04908ec6621d2f
RachelYang2 <lnfsyxr@163.com>
2019-10-17 16:42:20 +0800

    add no data hint

--- src/HouseTable.js
+++ src/HouseTable.js
@@ -14,13 +14,16 @@ class HouseTable extends React.Component {
         return (
             <div>
                 <Row>
+                    {houses && houses.length === 0 && <div>
+                        Sorry, no matched houses :(
+                    </div>}
                     {houses && houses.length > 0 && houses.map(house => (
                         <Col key={house.listing_id} span={8}>
                             <div className="info-block" style={{ padding: "1vw" }}>
                                 <Link to={`/house/${house.listing_id}`}><img src={house.picture_url} /></Link>
                                 <div>
                                     <p style={{ fontWeight: "bold" }}>{house.neighbourhood_group_cleansed && house.neighbourhood_group_cleansed.split(' ')[0]}&nbsp;- Singapore</p>
-                                    <p><Link to={`/house/${house.id}`}>{house.name}</Link></p>
+                                    <p><Link to={`/house/${house.listing_id}`}>{house.name}</Link></p>
                                     <p>&nbsp;{house.price}&nbsp;SGD/night </p>
                                     <div>
                                         {house.review_scores_rating && <StarRatings
